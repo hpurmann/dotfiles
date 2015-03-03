ddf82f5bf3efba5d89ba7d065d5af03e69a04356
Hendrik Purmann <h.purmann@googlemail.com>
2015-03-04 00:39:06 +0100

    Copy vimrc to nvimrc

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim tmux.conf zshrc gitconfig oh-my-zsh"    # list of files/folders to symlink in homedir
+files="vimrc vim nvimrc nvim tmux.conf zshrc gitconfig oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
