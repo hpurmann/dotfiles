cb78a5b85330b8161a884ce6cd434f4c8124c7bc
Hendrik Purmann <h.purmann@googlemail.com>
2015-04-26 14:14:23 +0200

    Clean up and refactor install script

--- install.sh
+++ install.sh
@@ -1,68 +1,33 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="vimrc vim nvimrc nvim tmux.conf zshrc gitconfig oh-my-zsh"
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim nvimrc nvim tmux.conf zshrc gitconfig oh-my-zsh"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# create backup directories
-cd "vim"
-mkdir "tmp"
-cd "tmp"
-mkdir "undo"
-mkdir "backup"
-mkdir "swap"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    # TODO if file exists
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
+function create_backup_dir {
+    mkdir -p $olddir
 }
 
-install_zsh
+function create_vim_dirs {
+    cd $dir
+    mkdir -p "vim/tmp/undo"
+    mkdir -p "vim/tmp/backup"
+    mkdir -p "vim/tmp/swap"
+}
+
+function backup_create_symlinks {
+    echo "Backing up dotfiles to $olddir"
+    cd ~
+    for file in $files; do
+        if [ -f ".$file" ]; then
+            mv ~/.$file $olddir
+        fi
+
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file ~/.$file
+    done
+}
+
+create_backup_dir
+create_vim_dirs
+backup_create_symlinks
